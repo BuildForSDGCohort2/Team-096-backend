b41a1356026b53a5876ca9e0c53e6ce9d8be65d9
Ahmad Adelaja <ahmadadelaja@gmail.com>
2020-09-06 20:21:40 +0100

    Modify manage.py to load appropriate settings file

--- gricapp/manage.py
+++ gricapp/manage.py
@@ -2,10 +2,20 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import socket
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
+    # If the host name starts with 'live',django_settings = "production"
+    if socket.gethostname().startswith('live'):
+        django_settings = 'config.settings.production'
+    # Else if host name starts with 'test', setdjango_settings = "test"
+    elif socket.gethostname().startswith('test'):
+        django_settings = 'config.settings.test'
+    else:
+        # If host doesn't match, assume it's a development server, setdjango_settings = "local"
+        django_settings = 'config.settings.local'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', django_settings)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
