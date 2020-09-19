131c5fe9fbda6ff92e39f8c6b2579824dc60467b
Ahmad Adelaja <ahmadadelaja@gmail.com>
2020-09-19 10:30:59 +0100

    Modify manage.py to run coverage for tests

--- gricapp/manage.py
+++ gricapp/manage.py
@@ -16,7 +16,22 @@ def main():
         # If host doesn't match, assume it's a development server, setdjango_settings = "local"
         django_settings = 'config.settings.local'
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', django_settings)
+
+    # Project Customization: run coverage.py around tests automatically
     try:
+        command = sys.argv[1]
+    except IndexError:
+        command = "help"
+
+    running_tests = (command == 'test')
+    if running_tests:
+        from coverage import Coverage  # pylint: disable=import-outside-toplevel
+        cov = Coverage()
+        cov.erase()
+        cov.start()
+
+    try:
+        # pylint: disable=import-outside-toplevel
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
@@ -25,6 +40,13 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
+    if running_tests:
+        cov.stop()
+        cov.save()
+        covered = cov.report()
+        if covered < 100:
+            sys.exit(1)
 
 
 if __name__ == '__main__':
