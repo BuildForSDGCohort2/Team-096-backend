259b78f9e02da301ba6b8be11d36ab3395005802
Ahmad Adelaja <ahmadadelaja@gmail.com>
2020-10-10 00:42:13 +0100

    Add authentication backend settings

--- gricapp/config/settings/production.py
+++ gricapp/config/settings/production.py
@@ -5,6 +5,12 @@ DEBUG = False
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-    )
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
 }
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
