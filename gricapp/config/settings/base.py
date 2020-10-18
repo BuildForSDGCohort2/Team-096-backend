3edd83ce63e9b97790bb501f8ea313fefba69a51
Ahmad Adelaja <ahmadadelaja@gmail.com>
2020-10-18 23:48:50 +0100

    change the staticfiles dir

--- gricapp/config/settings/base.py
+++ gricapp/config/settings/base.py
@@ -157,7 +157,7 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )  # specifications on what files to look for
-STATICFILES_DIRS = [str(ROOT_DIR.path('gricapp/static'))
+STATICFILES_DIRS = [str(ROOT_DIR.path('static'))
                     ]
 
 MEDIA_URL = '/media/'
